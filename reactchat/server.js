5e5cdcdc9750b7325decb8aef5ef1151f8f2fc20
unknown <alepore@umass.edu>
2017-11-24 17:09:28 -0500

    added file structure

--- reactchat/server.js
+++ reactchat/server.js
@@ -1,19 +1,28 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-
-// Serve up static assets (usually on heroku)
+const PORT = process.env.PORT || 3001;
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
-});
+// Add routes, both API and view
+app.use(routes);
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
+  {
+    useMongoClient: true
+  }
+);
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
