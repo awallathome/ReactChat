e8b69015738faf3a7b54730d0aed1713db354a41
awallathome <adam.wallis@me.com>
2017-12-02 12:00:00 -0500

    changed Route file names

--- reactchat/client/src/index.js
+++ reactchat/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from './Components/Router/router';
+import Routes from './Components/Routes/routes';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
